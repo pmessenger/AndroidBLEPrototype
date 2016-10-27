31397f742396b9db15f52226cd8fa6fa8402322f
Peter Messenger <peter.messenger@mysugr.com>
2016-10-27 17:37:31 +0200

    removed unused code

--- app/src/main/java/com/mysugr/rxbleprototype/GlucoseConstants.kt
+++ app/src/main/java/com/mysugr/rxbleprototype/GlucoseConstants.kt
@@ -12,11 +12,6 @@ object GlucometerConstants {
 	val CHARACTERISTIC_BG_CONTEXT_UUID = UUID.fromString("00002A34-0000-1000-8000-00805f9b34fb")
 	val CHARACTERISTIC_DEVICE_SERIALNUMBER = UUID.fromString("00002A25-0000-1000-8000-00805f9b34fb")
 
-	val AVIVA_CONNECT_PLIST_ID = "roche.accuchek.aviva.connect";
-	val PERFORMA_CONNECT_PLIST_ID = "roche.accuchek.performa.connect";
-	val BG5_PLIST_ID = "ihealth.wireless.smart"
-	val GL50_EVO_PLIST_ID = "beurer.gl50.evo"
-	const val Abbott_Freestyle_Libre = "abbott.freestyle.libre"
 
 
 	fun uuidToShortCode(uuid: UUID): String {
